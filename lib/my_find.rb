dbf7e731bb2f2aa598bb53f65015a2c66cefabed
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-19 20:51:16 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,20 @@
 require 'pry'
 
 def my_find(collection)
-  i = 0 
+  i = 0
   while i < collection.length
-    yield collection([i])
+    yield(collection[i])
+    #when the yield(1) is hit, it sends the 1 to the block, evaluates it, 
+    # and then sends the return value back to yeild. In this case, the block is sending
+    #true or false based on the element and the comparative/boolean evaluation statement 
+    #in the block. see ex block below
+    
     i += 1
   end
-  
+
 end
+
+
+#example block
+# my_find(collection) { |1| 1 % 3 ==0 and 1 % 5 ==0}
+#=> evaluates to false
